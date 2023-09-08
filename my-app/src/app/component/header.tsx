dbab3403fba5ec82562ae8cb08546b5ff05752c3
oubayid <oubaidbensaid18910@gmail.com>
2023-09-08 11:28:20 +0200

    Home and layout

--- my-app/src/app/component/header.tsx
+++ my-app/src/app/component/header.tsx
@@ -1,19 +1,122 @@
 import React from 'react'
 
-export default function Header() {
+function header() {
   return (
-   
-    <div className="w-96 h-12 pl-96 pr-32 py-3 bg-black justify-end items-center inline-flex">
-    <div className="justify-start items-start gap-56 inline-flex">
-      <div className="justify-start items-center gap-2 flex">
-        <div className="w-96 h-4 text-neutral-50 text-sm font-normal leading-tight">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</div>
-        <div className="text-center text-neutral-50 text-sm font-semibold underline leading-normal">ShopNow</div>
+	<div>
+<div className="w-[1900px] w-[1440px] h-12 pl-[445px] pr-[136px] py-3 bg-black justify-end items-center inline-flex">
+  <div className="justify-start items-start gap-[531px] inline-flex">
+    <div className="justify-start items-center gap-2 flex">
+      <div className="w-[474px] h-[18px] text-neutral-50 text-sm font-normal leading-[21px]">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</div>
+      <div className="text-center text-neutral-50 text-sm font-semibold underline leading-normal">ShopNow</div>
+    </div>
+    <div className="justify-center items-center gap-[5px] flex">
+      <div className="text-neutral-50 text-sm font-normal leading-[21px]">English</div>
+      <div className="w-6 h-6 relative"></div>
+    </div>
+  </div>
+</div>
+
+
+{/*footer*/}
+<div className=' w-[0px] h-[0px] pt-20 pb-6 bg-black flex-col justify-end items-center gap-[60px] inline-flex" style={{ marginTop: "100px" }}'>
+<div    style={{ marginTop: "4000px" }}  className="  w-[1900px] h-[420px] pt-20 pb-0 bg-black flex-col justify-end items-center gap-[50px] inline-flex ">
+  <div className=" justify-center items-start gap-[87px] inline-flex">
+    <div className="flex-col justify-start items-start gap-4 inline-flex">
+      <div className="flex-col justify-start items-start gap-6 flex">
+        <div className="flex-col justify-start items-start gap-6 flex">
+          <div className="w-[118px] h-6 justify-center items-center inline-flex">
+            <div className="text-neutral-50 text-2xl font-bold leading-normal tracking-wide">Exclusive</div>
+          </div>
+          <div className="text-neutral-50 text-xl font-medium leading-7">Subscribe</div>
+        </div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">Get 10% off your first order</div>
       </div>
-      <div className="justify-center items-center gap-1 flex">
-        <div className="text-neutral-50 text-sm font-normal leading-tight">English</div>
-        <div className="w-6 h-6 relative" />
+      <div className="w-[217px] pl-4 py-3 rounded border border-neutral-50 justify-start items-center gap-8 inline-flex">
+        <div className="opacity-40 text-neutral-50 text-base font-normal leading-normal">Enter your email</div>
+        <div className="w-6 h-6 relative"></div>
+      </div>
+    </div>
+    <div className="flex-col justify-start items-start gap-6 inline-flex">
+      <div className="text-neutral-50 text-xl font-medium leading-7">Support</div>
+      <div className="flex-col justify-start items-start gap-4 flex">
+        <div className="w-[175px] text-neutral-50 text-base font-normal leading-normal">111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">exclusive@gmail.com</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">+88015-88888-9999</div>
+      </div>
+    </div>
+    <div className="flex-col justify-start items-start gap-6 inline-flex">
+      <div className="text-neutral-50 text-xl font-medium leading-7">Account</div>
+      <div className="flex-col justify-start items-start gap-4 flex">
+        <div className="text-neutral-50 text-base font-normal leading-normal">My Account</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">Login / Register</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">Cart</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">Wishlist</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">Shop</div>
+      </div>
+    </div>
+    <div className="flex-col justify-start items-start gap-6 inline-flex">
+      <div className="text-neutral-50 text-xl font-medium leading-7">Quick Link</div>
+      <div className="flex-col justify-start items-start gap-4 flex">
+        <div className="text-neutral-50 text-base font-normal leading-normal">Privacy Policy</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">Terms Of Use</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">FAQ</div>
+        <div className="text-neutral-50 text-base font-normal leading-normal">Contact</div>
+      </div>
+    </div>
+    <div className="flex-col justify-start items-start gap-6 inline-flex">
+      <div className="flex-col justify-start items-start gap-6 flex">
+        <div className="text-neutral-50 text-xl font-medium leading-7">Download App</div>
+        <div className="flex-col justify-start items-start gap-2 flex">
+          <div className="opacity-70 text-neutral-50 text-xs font-medium leading-[18px]">Save $3 with App New User Only</div>
+          <div className="justify-start items-center gap-2 inline-flex">
+            <div className="w-20 h-20 p-0.5 bg-black justify-center items-center flex">
+              <img className="w-[76px] h-[76px] border border-white" src="https://via.placeholder.com/76x76" />
+            </div>
+            <div className="flex-col justify-start items-start gap-1 inline-flex">
+              <div className="w-[110px] h-10 relative bg-black">
+                <img className="w-[104px] h-[30px] left-[3px] top-[5px] absolute rounded border border-neutral-50" src="https://via.placeholder.com/104x30" />
+              </div>
+              <div className="w-[110px] h-10 p-[3px] bg-black justify-center items-center inline-flex">
+                <img className="w-[104px] h-[34px] rounded border border-white" src="https://via.placeholder.com/104x34" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="justify-start items-start gap-6 inline-flex">
+        <div className="w-6 h-6 relative"></div>
+        <div className="w-6 h-6 pr-[1.94px] justify-start items-center flex">
+          <div className="w-[27.06px] h-6 relative">
+          </div>
+        </div>
+        <div className="w-6 h-6 p-[3px] justify-center items-center flex">
+          <div className="w-[18px] h-[18px] relative">
+          </div>
+        </div>
+        <div className="w-6 h-6 relative"></div>
       </div>
     </div>
   </div>
+  <div className="opacity-40 flex-col justify-start items-center gap-4 inline-flex">
+    <div className="w-[1440px] h-[0px] opacity-50 justify-center items-center inline-flex">
+      <div className="w-[1440px] h-[0px] opacity-40 border border-white"></div>
+    </div>
+    <div className="opacity-60 justify-start items-center gap-3 inline-flex">
+      <div className="justify-start items-center gap-1.5 flex">
+        <div className="w-5 h-5 p-[1.67px] justify-center items-center flex">
+          <div className="w-[16.67px] h-[16.67px] relative">
+          </div>
+        </div>
+        <div className="text-white text-base font-normal leading-normal">Copyright Rimel 2022. All right reserved</div>
+      </div>
+    </div>
+  </div>
+</div>
+	
+	
+	</div>
+	</div>
   )
 }
+
+export default header
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,238 @@
-import Image from 'next/image'
-import Header from "@/app/component/header"
+import Image from "next/image";
+import Header from "@/app/component/header";
+import data from "./../../data.json"
+
+
+
 export default function Home() {
-  return (<div>
-    {/* <Header/> */}
+  return (
+    <div>
+      {/* <Header/> */}
     
+      <div
+
+      
+        style={{ marginTop: "10px", marginLeft: "40px" }}
+        className="w-[217px] h-[344px] flex-col justify-start items-start gap-4 inline-flex"
+      >
+        <div className="justify-start items-start gap-[51px] inline-flex">
+          <div className="text-center text-black text-base font-normal leading-normal">
+            Woman’s Fashion
+          </div>
+          <div className="w-6 h-6 pl-1.5 pr-[5.27px] pt-2 pb-[8.22px] origin-top-left -rotate-90 justify-center items-center flex"></div>
+        </div>
+        <div className="justify-start items-start gap-[81px] inline-flex">
+          <div className="text-center text-black text-base font-normal leading-normal">
+            Men’s Fashion
+          </div>
+          <div className="w-6 h-6 relative origin-top-left -rotate-90"></div>
+        </div>
+        <div className="text-center text-black text-base font-normal leading-normal">
+          Electronics
+        </div>
+        <div className="text-center text-black text-base font-normal leading-normal">
+          Home & Lifestyle
+        </div>
+        <div className="text-center text-black text-base font-normal leading-normal">
+          Medicine
+        </div>
+        <div className="text-center text-black text-base font-normal leading-normal">
+          Sports & Outdoor
+        </div>
+        <div className="text-center text-black text-base font-normal leading-normal">
+          Baby’s & Toys
+        </div>
+        <div className="text-center text-black text-base font-normal leading-normal">
+          Groceries & Pets
+        </div>
+        <div className="text-center text-black text-base font-normal leading-normal">
+          Health & Beauty
+        </div>
+        
+      </div>
+      <div
+        style={{ marginLeft: "40px", marginTop: "90px" }}
+        className=" w-5 h-10 relative"
+      >
+        <div className=" w-5 h-10 left-0 top-0 absolute bg-red-500 rounded"></div>
+      </div>
+      <div
+        style={{ marginLeft: "80px", marginTop: "-30px" }}
+        className="text-red-500 text-base font-semibold leading-tight"
+      >
+        Today’s
+      </div>
+      <div
+        style={{ marginLeft: "40px", marginTop: "40px" }}
+        className="text-black text-4xl font-semibold leading-10 tracking-wider"
+      >
+        Flash Sales
+      </div>
+
+      <div className="">
+        <div className="px-3 py-1 left-[12px] top-[12px] absolute bg-red-500 rounded justify-center items-center gap-2.5 inline-flex">
+          <div className="text-neutral-50 text-xs font-normal leading-none">
+            -40%
+          </div>
+        </div>
+        <div className="left-[224px] top-[12px] absolute flex-col justify-start items-start gap-2 inline-flex">
+          <div className="w-8 h-8 relative">
+            <div className="w-8 h-8 left-0 top-0 absolute bg-white rounded-full"></div>
+            <div className="w-6 h-6 left-[5px] top-[5px] absolute"></div>
+          </div>
+          <div className="w-8 h-8 relative">
+            <div className="w-8 h-8 left-0 top-0 absolute bg-white rounded-full"></div>
+            <div className="w-6 h-6 px-0.5 py-1 left-[5px] top-[5px] absolute justify-center items-center inline-flex">
+              <div className="w-5 h-3.5 relative"></div>
+            </div>
+          </div>
+        </div>
+        <div className="w-48 h-44 px-2 py-3.5 left-[40px] top-[35px] absolute justify-center items-center inline-flex">
+          <img
+            style={{ marginLeft: "400px", marginTop: "1500px" }}
+            className="w-44 h-36 "
+            src="https://static-01.daraz.com.np/p/ca889918e9e503797cc5863a09b58eae.jpg"
+          />
+        </div>
+      </div>
+      <div
+        style={{ marginLeft: "245px", marginTop: "250px" }}
+        className="text-black text-base font-medium leading-normal"
+      >
+        HAVIT HV-G92 Gamepad
+      </div>
+      <div
+        style={{ marginLeft: "250px" }}
+        className="text-red-500 text-base font-medium leading-normal"
+      >
+        $120
+      </div>
+      <div
+        style={{ marginLeft: "370px", marginTop: "-22px" }}
+        className="opacity-50 text-black text-base font-medium line-through leading-normal"
+      >
+        $160
+      </div>
+
+      <div
+        style={{ marginLeft: " 850px", marginTop: "90px" }}
+        className="w-60 h-14 px-12 py-4 bg-red-500 rounded justify-center items-center gap-2.5 inline-flex"
+      >
+        <div className="text-neutral-50 text-base font-medium leading-normal">
+          View All Products
+        </div>
+      </div>
+      <div className="w-96 h-px origin-top-left rotate-180 opacity-30 border border-black"></div>
+
+      <div
+        style={{ marginLeft: " 40px", marginTop: "90px" }}
+        className="w-5 h-10 relative"
+      >
+        <div className="w-5 h-10 left-0 top-0 absolute bg-red-500 rounded"></div>
+      </div>
+      <div
+        style={{ marginLeft: " 80px", marginTop: "-30px" }}
+        className="text-red-500 text-base font-semibold leading-tight"
+      >
+        Categories
+      </div>
+      <div
+        style={{ marginLeft: " 40px", marginTop: "50px" }}
+        className="text-black text-4xl font-semibold leading-10 tracking-wider"
+      >
+        Browse By Category
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <div style={{ marginLeft: " 500px", marginTop: "-60px" }} >
+      <div className="w-44 h-36 px-14 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-end gap-4 inline-flex">
+        <div className="w-14 h-14 relative flex-col justify-start items-start flex"></div>
+        <div className="text-black text-base font-normal leading-normal">
+          Phones
+        </div>
+      </div>
+
+      <div className="w-44 h-36 px-10 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
+        <div className="w-14 h-14 relative flex-col justify-start items-start flex"></div>
+        <div className="text-black text-base font-normal leading-normal">
+          Computers
+        </div>
+      </div>
+      <div className="w-44 h-36 pl-8 pr-9 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
+  <div className="w-14 h-14 relative flex-col justify-start items-start flex">
+    <div className="w-2.5 h-3 justify-start items-end gap-1 inline-flex">
+      <div className="w-3 h-px origin-top-left rotate-90 border border-black"></div>
+      <div className="w-2 h-px origin-top-left rotate-90 border border-black"></div>
+      <div className="w-2.5 h-px origin-top-left rotate-90 border border-black"></div>
     </div>
-  )
+  </div>
+  <div  className="text-black text-base font-normal leading-normal">SmartWatch</div>
+</div>
+<div className="w-44 h-36 pl-8 pr-9 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
+  <div className="w-14 h-14 relative flex-col justify-start items-start flex"></div>
+  <div className="text-black text-base font-normal leading-normal">HeadPhones</div>
+</div>
+<div className="w-44 h-36 pl-14 pr-12 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
+  <div className="w-14 h-14 relative flex-col justify-start items-start flex"></div>
+  <div className="text-black text-base font-normal leading-normal">Gaming</div>
+</div>
+</div>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div  className="w-5 h-10 relative">
+  <div   style={{ marginLeft: " 40px", marginTop: "90px" }} className="w-5 h-10 left-0 top-0 absolute bg-red-500 rounded"></div>
+</div>
+<div   style={{ marginLeft: " 80px", marginTop: "60px" }} className="text-red-500 text-base font-semibold leading-tight">This Month</div>
+   
+<div  style={{ marginLeft: " 40px", marginTop: "60px" }} className="text-black text-4xl font-semibold leading-10 tracking-wider">Best Selling Products</div>
+  
+  <img
+            style={{ marginLeft: "230px", marginTop: "150px" }}
+            className="w-44 h-36"
+            src="https://cdni.llbean.net/is/image/wim/260347_2772_41?hei=1095&wid=950&resMode=sharp2&defaultImage=llbprod/A0211793_2"
+          />
+  <div  style={{ marginLeft: "260px", marginTop: "0px" }} className="text-black text-base font-medium leading-normal">The north coat</div>
+
+  <div style={{ marginLeft: "230px"}} className="text-red-500 text-base font-medium leading-normal">$260</div>
+
+  <div style={{ marginLeft: "360px" , marginTop:"-25px"}} className="opacity-50 text-black text-base font-medium line-through leading-normal">$360</div>
+
+  <img style={{ marginLeft: "360px" , marginTop:"-25px"}} src=""/>
+  
+    <div   style={{ marginLeft: " 40px", marginTop: "2500px" }} className="w-5 h-10 left-0 top-0 absolute bg-red-500 rounded"></div>
+    <div style={{ marginLeft: " 80px", marginTop: "280px" }} className="text-black text-4xl font-semibold leading-10 tracking-wider">Explore Our Products</div>
+    <img  style={{ marginLeft: "230px", marginTop: "150px" }} className="w-35 h-44" src="https://www.hillspet.com/content/dam/pim/hills/en_us/sd/dry/sd-canine-adult-large-breed-dry-productShot_zoom.jpg" />
+    <div  style={{ marginLeft: "240px", marginTop: "0px" }} className="text-black text-base font-medium leading-normal">Bread Dry dog Food</div>
+    <div style={{ marginLeft: "300px"}} className="text-red-500 text-base font-medium leading-normal">$260</div>
+
+    <div style={{ marginLeft: "500px" , marginTop:"900px"}} className="w-20 h-20 relative">
+  <div className="w-20 h-20 left-0 top-0 absolute opacity-30 bg-zinc-800 rounded-full"></div>
+  <div className="w-14 h-14 left-[11px] top-[11px] absolute bg-black rounded-full"></div>
+<br/>
+<br/>
+
+</div>
+
+<div style={{ marginLeft: "1000px" , marginTop:"-80px"}} className="w-20 h-20 relative">
+  <div className="w-20 h-20 left-0 top-0 absolute opacity-30 bg-zinc-800 rounded-full"></div>
+  <div className="w-14 h-14 left-[11px] top-[11px] absolute bg-black rounded-full"></div>
+</div>
+
+<div style={{ marginLeft: "1500px" , marginTop:"-70px"}} className="w-20 h-20 relative">
+  <div className="w-20 h-20 left-0 top-0 absolute opacity-30 bg-zinc-800 rounded-full"></div>
+  <div className="w-14 h-14 left-[11px] top-[11px] absolute bg-black rounded-full"></div>
+</div>
+  </div>
+
+
+
+
+
+  );
 }
