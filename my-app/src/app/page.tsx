e6406aeec21944eff42481e9dc41ec2799c01b23
oubayid <oubaidbensaid18910@gmail.com>
2023-09-11 23:47:22 +0200

    Home 70% contactUs 90%

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,9 +3,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
+import todays from "../../todays.json"
+import bestSelling from "../../bestSelling.json"
+import ourProduct from "../../ourProduct.json"
+import { stringify } from "querystring";
+console.log(bestSelling)
+console.log(ourProduct)
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [todaysData , setTodaysData]=useState([])
+  const [bestSellingData , setbestSellingData]=useState([])
+  const [ourProductData , setOurProductData]=useState([])
+
   console.log(products)
 
  
@@ -16,6 +26,10 @@ export default function Products() {
         const response = await axios.get("http://localhost:3000/api/products");
         const productsData = response.data;
         setProducts(productsData);
+        setTodaysData(todays);
+        setbestSellingData(bestSelling)
+        setOurProductData(ourProduct)
+        console.log("those product of todays " , todays)
       
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -99,7 +113,7 @@ export default function Products() {
 </div>
 
 {/*product of today's*/}
-{products.map((product)=>(
+{todaysData.map((product)=>(
   <div   className="w-60 h-10 px-0 py-4  rounded justify-center items-center gap-2.5 inline-flex" style={{ marginLeft:"50px" , marginTop:"50px" }}   >
 <div  className="w-64 h-80 flex-col justify-start items-start gap-4 inline-flex">
   <div className="w-64 h-64 relative bg-neutral-0 rounded">
@@ -137,9 +151,9 @@ export default function Products() {
 ))}
 
 {/*view All products*/}
-<div  style={{marginTop:"500px" , marginLeft:"800px"}}>
+<div  style={{marginTop:"450px" , marginLeft:"800px"}}>
 <div className="w-60 h-14 px-12 py-4 bg-red-500 rounded justify-center items-center gap-2.5 inline-flex">
-  <button  className="text-neutral-50 text-base font-medium leading-normal">View All Products</button>
+  <button  className="text-neutral-50 text-base font-medium leading-normal" >View All Products</button>
 </div>
 </div>
 
@@ -172,37 +186,37 @@ export default function Products() {
   <div style={{marginTop:"80px" , marginLeft:"300px"}} className="justify-start items-start gap-7 inline-flex">
     <div className="w-44 h-36 px-14 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-end gap-4 inline-flex">
       <div className="w-14 h-14 relative flex-col justify-start items-start flex">
-        <div className="w-6 h-px border border-black"></div>
+        <div></div>
       </div>
       <img src="https://static.vecteezy.com/system/resources/previews/006/329/501/original/icon-phone-call-isolated-white-background-free-vector.jpg"/>
       <div  className="text-black text-base font-normal leading-normal">Phones</div>
 
     </div>
     
-    <div className="w-44 h-36 px-10 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
+    <div className="w-40 h-36 px-10 py-6  rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
       <div className="w-14 h-14 relative flex-col justify-start items-start flex"></div>
       <img src="https://t3.ftcdn.net/jpg/02/65/17/46/360_F_265174698_QwmmvOlQmVrA9vMxwVyFf82wZLtSIkQY.jpg "/>
   
       <div className="text-black text-base font-normal leading-normal">Computers</div>
     
     </div>
-    <div className="w-44 h-36 pl-8 pr-9 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
+    <div  className="w-40 h-36 pl-12 pr-11 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
       <div className="w-14 h-14 relative flex-col justify-start items-start flex">
         <div className="w-2.5 h-3 justify-start items-end gap-1 inline-flex">
-          <div className="w-3 h-px origin-top-left rotate-90 border border-black"></div>
-          <div className="w-2 h-px origin-top-left rotate-90 border border-black"></div>
-          <div className="w-2.5 h-px origin-top-left rotate-90 border border-black"></div>
+          <div></div>
+          <div ></div>
+          <div ></div>
         </div>
       </div>
       <img src="https://static.vecteezy.com/system/resources/previews/006/659/379/original/smart-watch-outline-icon-illustration-on-isolated-white-background-suitable-for-fashion-accessories-wristwatch-icon-free-vector.jpg"/>
       <div className="text-black text-base font-normal leading-normal">SmartWatch</div>
     </div>
-    <div className="w-44 h-36 px-12 py-6  rounded shadow flex-col justify-center items-center gap-4 inline-flex">
+    <div className="w-40 h-36 pl-12 pr-11 py-6  rounded shadow flex-col justify-center items-center gap-4 inline-flex">
       <div className="w-14 h-14 relative flex-col justify-start items-start flex"></div>
       <img src="https://img.myloview.com/canvas-prints/camera-icon-vector-camera-icon-isolated-on-white-background-camera-icon-simple-and-modern-for-app-web-and-design-700-245051969.jpg"/>
       <div className="text-black-50 text-base font-normal leading-normal">Camera</div>
     </div>
-    <div className="w-44 h-36 pl-8 pr-9 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
+    <div className="w-40 h-36 pl-12 pr-11 py-6 rounded border border-black border-opacity-30 flex-col justify-center items-center gap-4 inline-flex">
       <div className="w-14 h-14 relative flex-col justify-start items-start flex"></div>
     <img src="https://static.vecteezy.com/system/resources/previews/009/688/816/original/sign-of-the-headphones-symbol-is-isolated-on-a-white-background-headphones-icon-color-editable-vector.jpg"/>
       <div className="text-black text-base font-normal leading-normal">HeadPhones</div>
@@ -217,7 +231,7 @@ export default function Products() {
 </button>
 
 {/*best selling product*/}
-<div style={{marginTop:"150px" , marginLeft:"80px"}}>
+<div  style={{marginTop:"150px" , marginLeft:"80px"}}>
 <div className="w-96 h-28 justify-start items-end gap-96 inline-flex">
   <div className="flex-col justify-start items-start gap-5 inline-flex">
     <div className="justify-start items-center gap-4 inline-flex">
@@ -235,7 +249,9 @@ export default function Products() {
 </div>
 
 
-{/*first product of best selling*/}
+{/*best selling*/}
+{bestSellingData.map((product)=>(
+
 <div  style={{ marginLeft:"50px" , marginTop:"50px" }}  >
 <div className="w-64 h-80 flex-col justify-start items-start gap-4 inline-flex">
   <div className="w-64 h-64 relative bg-neutral-0 rounded">
@@ -253,13 +269,13 @@ export default function Products() {
       </div>
     </div>
     <div className="w-48 h-44 px-6 py-4 left-[40px] top-[35px] absolute justify-center items-center inline-flex">
-      <img  className="w-36 h-36" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOvF2-YCwvPvKZfCAUQFC6-ajdpnuVNy2zjGJeKzTJ&s" />
+      <img  className="w-36 h-36" src={product.image} />
     </div>
   </div>
   <div className="flex-col justify-start items-start gap-2 flex">
-    <div  style={{ marginLeft:"80px"  }} className="text-black text-base font-medium leading-normal">The north coat</div>
+    <div  style={{ marginLeft:"80px"  }} className="text-black text-base font-medium leading-normal">{product.name}</div>
     <div className="justify-start items-start gap-3 inline-flex">
-      <div style={{ marginLeft:"80px"  }} className="text-red-500 text-base font-medium leading-normal">$260</div>
+      <div style={{ marginLeft:"80px"  }} className="text-red-500 text-base font-medium leading-normal">{product.price}</div>
       <div className="opacity-50 text-black text-base font-medium line-through leading-normal">$360</div>
     </div>
     <div className="justify-start items-start gap-2 inline-flex">
@@ -268,12 +284,15 @@ export default function Products() {
   </div>
 </div>
 </div>
+))}
+
 
 {/*explore our product*/}
+
 <div style={{marginTop:"150px" , marginLeft:"80px"}}>
 <div className="w-96 h-28 flex-col justify-start items-start gap-5 inline-flex">
   <div className="justify-start items-center gap-4 inline-flex">
-    <div className="w-5 h-10 relative">
+    <div className="w-4 h-10 relative">
       <div className="w-5 h-10 left-0 top-0 absolute bg-red-500 rounded"></div>
     </div>
     <div className="text-red-500 text-base font-semibold leading-tight">Our Products</div>
@@ -281,41 +300,48 @@ export default function Products() {
   <div className="text-black text-0x6 font-semibold leading-10 tracking-wider">Explore Our Products</div>
 </div>
 </div>
-{/*explore the products*/}
-<div style={{marginTop:"150px" , marginLeft:"80px"}}>
-<div className="w-64 h-80 flex-col justify-start items-start gap-4 inline-flex">
-  <div className="w-64 h-64 relative bg-neutral-00 rounded">
-    <div className="left-[224px] top-[12px] absolute flex-col justify-start items-start gap-2 inline-flex">
-      <div className="w-8 h-8 relative">
-        <div className="w-8 h-8 left-0 top-0 absolute bg-white rounded-full"></div>
-        <div className="w-6 h-6 left-[5px] top-[5px] absolute"></div>
-      </div>
-      <div className="w-8 h-8 relative">
-        <div className="w-8 h-8 left-0 top-0 absolute bg-white rounded-full"></div>
-        <div className="w-6 h-6 px-0.5 py-1 left-[5px] top-[5px] absolute justify-center items-center inline-flex">
-          <div className="w-5 h-3.5 relative">
+{/*our product*/}
+
+
+{ourProductData.map((product, index) => (
+  <div key={index} style={{ marginTop: "150px", marginLeft: "80px" }}>
+    <div className="w-64 h-80 flex-col justify-start items-start gap-4 inline-flex">
+      <div className="w-64 h-64 relative bg-neutral-00 rounded">
+        <div className="left-[224px] top-[12px] absolute flex-col justify-start items-start gap-2 inline-flex">
+          <div className="w-6 h-8 relative">
+            <div className="w-8 h-8 left-0 top-0 absolute bg-white rounded-full"></div>
+            <div className="w-6 h-6 left-[5px] top-[5px] absolute"></div>
           </div>
+          <div className="w-8 h-8 relative">
+            <div className="w-8 h-8 left-0 top-0 absolute bg-white rounded-full"></div>
+            <div className="w-6 h-6 px-0.5 py-1 left-[5px] top-[5px] absolute justify-center items-center inline-flex">
+              <div className="w-5 h-3.5 relative"></div>
+            </div>
+          </div>
+        </div>
+        <div className="w-48 h-44 left-[40px] top-[35px] absolute "></div>
+        <img className="w-44 h-40 left-[49px] top-[66px] absolute  " src={product.image} alt={product.name} />
+      </div>
+      <div className="flex-col justify-start items-start gap-2 flex">
+        <div style={{ marginTop: "150px", marginLeft: "50px" }} className="text-black text-base font-medium leading-normal">
+          {product.name}
+        </div>
+        <div className="justify-start items-center gap-2 inline-flex">
+          <div className="justify-start items-start gap-3 flex">
+            <div style={{ marginTop: "10px", marginLeft: "110px" }} className="text-red-500 text-base font-medium leading-normal">
+              {product.price}
+            </div>
+          </div>
+          <div className="justify-start items-start gap-2 flex"></div>
         </div>
       </div>
     </div>
-    <div className="w-48 h-44 left-[40px] top-[35px] absolute"></div>
-    <img  className="w-44 h-40 left-[49px] top-[46px] absolute" src="https://curology.com/static/587736447cdb7f3881874042d2f4575c/7cad7/completeSetMobile%402x.png" />
   </div>
-  <div className="flex-col justify-start items-start gap-2 flex">
-    <div  style={{marginTop:"150px" , marginLeft:"50px"}} className="text-black text-base font-medium leading-normal">Curology Product Set </div>
-    <div className="justify-start items-center gap-2 inline-flex">
-      <div className="justify-start items-start gap-3 flex">
-        <div   style={{marginTop:"10px" , marginLeft:"110px"}} className="text-red-500 text-base font-medium leading-normal">$500</div>
-      </div>
-      <div className="justify-start items-start gap-2 flex">
-      </div>
-    </div>
-  </div>
+))}
+<div style={{ marginTop: "80px", marginLeft: "800px" }} className="w-60 h-14 px-12 py-4 bg-red-500 rounded justify-center items-center gap-2.5 inline-flex">
+  <div className=" text-white text-neutral-50 text-base font-medium leading-normal">View All Products</div>
 </div>
-</div>
-<div style={{marginTop:"80px" , marginLeft:"800px"}} className="w-60 h-14 px-12 py-4 bg-red-500 rounded justify-center items-center gap-2.5 inline-flex">
-  <div className= " text-white-500 text-neutral-50 text-base font-medium leading-normal">View All Products</div>
-</div>
+
 
 {/*featured*/}
 <div style={{marginLeft:"80px"}}>
@@ -386,7 +412,9 @@ export default function Products() {
   </div>
 </div>
 </div>
+
     </div>
+    
   )
 }
 
